cbd6e99b6f2da5c15d42bb7a4a2c6ede8cc33210
devinziegler <devin.ziegler@mymail.champlain.edu>
2025-12-05 11:21:36 -0500

    Milestone 7.2

--- SYS-350/HyperV-Automation/menu.ps1
+++ SYS-350/HyperV-Automation/menu.ps1
@@ -5,29 +5,99 @@ function Show-Menu {
 
     Clear-Host
     Write-Host "==================$title=================="
-    Write-Host "1: Option 1"
-    Write-Host "2: Option 2"
-    Write-Host "3: Option 3"
-    Write-Host "4: Option 4"
-    Write-Host "5: Option 5"
+    Write-Host "1: VM Quick Info"
+    Write-Host "2: VM 5 Details"
+    Write-Host "3: Restore From Latest Snapshot"
+    Write-Host "4: Create a Full Clone of a VM"
+    Write-Host "5: Set VM Memory"
     Write-Host
     Write-Host "==================$title=================="
 
 }
 
+function get-data {
+    Get-VM | Select-Object Name, State,
+        @{Name="IP Address";Expression={(Get-VMNetworkAdapter -VMName $_.Name).IPAddresses}} | Format-Table -AutoSize
+    
+}
+function get-info {
+    param (
+        [string]$Name
+    )
+    Get-VM -Name $Name | Select-Object Name, ComputerName, Version, Uptime,
+        @{Name="CPU Usage";Expression={(Get-VM -Name $_.Name).CPUUsage}},
+        @{Name="Assigned Memory";Expression={(Get-VM -Name $_.Name).MemoryAssigned}}
+}
+function restore-snapsot {
+    param (
+        [string]$Name
+    )
+    $latest_snapshot = Get-VMSnapshot -VMName $Name |
+    Sort-Object CreationTime -Descending | Select-Object -First 1
+    Restore-VMSnapshot -Name $latest_snapshot.Name -VMName $Name
+}
+function clone-vm {
+    param (
+        [string]$Name,
+        [string]$CloneVMName
+    )
+    $ExportFolder = "C:\Exports"
+    $CloneFolder = "C:\Clone VM Storage"
+
+    If (Test-Path $CloneFolder) {
+        Write-Warning "Clone Folder: $CloneFolder already exists. Aborting Script ..."
+        Break
+    }
+    # Export the Source VM
+    Export-VM $Name -Path $ExportFolder
+
+    # Import Exported VM,
+    Get-ChildItem -Path $ExportFolder -File -Name
+    Import-VM -Path $ExportFolder -Copy -GenerateNewId |
+        Rename-VM -NewName $CloneVMName
+}
+function set-memory {
+    param (
+        [string]$Name
+        
+    )
+}
+
 function main {
     while($true) {
         Show-Menu
-        $input = Read-Host "Please select an option"
-        switch ($input) {
+        $choice = Read-Host "Please select an option"
+        switch ($choice) {
             '1' {
-                Write-Host "option 1"
+                Write-Host "Display VM Information"
+                get-data
+
             }
             '2' {
-                Write-Host "option 2"
+                Write-Host "Display VM Summary ..."
+                $vmName = Read-Host "Please Enter a VM Name"
+                get-info -Name $vmName
             }
-            '3' {
-                Write-Host "option 3"
+            '3'{
+                Write-Host "Restore To Latest Snapshot ..."
+                $vmName = Read-Host "Please Enter a VM Name"
+                restore-snapsot -Name $vmName
+            }
+            '4'{
+                Write-host "Creating Full Clone ..."
+                $vmName = Read-Host "Enter a VM Name"
+                $CloneVMName = Read-Host "Enter a Vm Name for the Clone"
+                clone-vm -Name $vmName -CloneVMName $CloneVMName
+            }
+            '5'{
+                Write-host "Change Ram Count"
+                $vmName = Read-Host "Enter a VM Name"
+                set-memory -Name $vmName
+            }
+
+            'q' {
+                Write-Host "Exiting ..."
+                return
             }
             default {
                 Write-Host "Invalid option, please try again."
